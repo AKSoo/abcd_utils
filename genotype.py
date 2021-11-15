6e14b2b0e1174b8455c5c6978fc25b38d1fff82a
Sin Kim <kimsin98@gmail.com>
2021-11-15 16:52:59 +0900

    reference rs match function

--- genotype.py
+++ genotype.py
@@ -1,15 +1,50 @@
 from pathlib import Path
 import pandas as pd
 
+GWAS_DIR = Path('inputs/GWAS')
 PRS_DIR = Path('outputs/prs')
+
+BIM_COLS = ['CHR', 'SNP', 'POS', 'BP', 'A1', 'A2']
 
 PHENOTYPES = {
     'ASD': {'n': 46350}
 }
 
+
+def reference_ids(genes, reference, swap=False):
+    """
+    Get SNP IDs from a reference for given genes.
+    Matches
+        CHR: chromosome (no X), BP: base pair
+        A1: minor allele, A2: major allele
+
+    Params:
+        genes: pd.DataFrame with columns CHR, BP, A1, A2, SNP
+        reference: pd.DataFrame with columns CHR, BP, A1, A2, SNP
+        swap: bool, try swapping A1 and A2 for match.
+            rs IDs for swapped matches are reversed ('...sr').
+
+    Returns:
+        ids: pd.Series of str
+    """
+    reference = (reference.dropna().astype({'CHR': int, 'BP': int})
+                 .set_index(['CHR', 'BP']).sort_index())
+
+    def query_id(row):
+        if (row['CHR'], row['BP']) in reference.index:
+            match = reference.loc[(row['CHR'], row['BP'])]
+            if match['A1'] == row['A1'] and match['A2'] == row['A2']:
+                return match['SNP']
+            if swap and match['A1'] == row['A2'] and match['A2'] == row['A1']:
+                return match['SNP'][::-1]
+        return row['SNP']
+
+    ids = genes.apply(query_id, axis=1)
+    return ids
+
+
 def subs_index(index):
     return index.str.split('_', n=1).str[1]
-
 
 def load_prscores(pheno):
     """
