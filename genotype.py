be7e9a3300d22415918b9d4099f196376ac91872
Sin Kim <kimsin98@gmail.com>
2021-11-23 15:17:40 +0900

    utils: PRS from plink output

--- genotype.py
+++ genotype.py
@@ -53,13 +53,13 @@ def load_prscores(pheno):
         scores: DataFrame indexed by subject
     """
     scores_dir = PRS_DIR / pheno / 'scores'
-    cols_dict = {'#IID': None, 'SCORE1_SUM': pheno}
+    cols_dict = {'IID': None, 'SCORESUM': pheno}
 
     scores = 0
-    for score_path in scores_dir.glob('*.sscore'):
-        scores += pd.read_table(score_path, index_col='#IID',
+    for score_path in scores_dir.glob('*.profile'):
+        scores += pd.read_table(score_path, index_col='IID',
                                 usecols=cols_dict.keys())
 
-    scores.index = subs_index(scores.index).rename(cols_dict['#IID'])
+    scores.index = subs_index(scores.index).rename(cols_dict['IID'])
     scores = scores.rename(columns=cols_dict)
     return scores
